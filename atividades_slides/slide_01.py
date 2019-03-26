7614371256ab9dcce1e1fde69ead30b6cf609d84
Henrique Garcia <enriqe_garcia15@hotmail.com>
2019-03-26 08:00:15 -0300

    questão 01

--- atividades_slides/slide_01.py
+++ atividades_slides/slide_01.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import requests
 
-url = str(input('Insira a url: '))
-print(url)
+url = str(input('Insira a url\nEx: www.uol.com.br\n>>>  '))
+response = requests.get('http://%s'%(url))
+
+print(response.status_code)
