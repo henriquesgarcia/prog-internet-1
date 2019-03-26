d5ecc671e1d0d9c302665c89300b023773291955
Henrique Garcia <enriqe_garcia15@hotmail.com>
2019-03-26 11:37:02 -0300

    questao 02

--- atividades_slides/slide_01.py
+++ atividades_slides/slide_01.py
@@ -9,9 +9,15 @@ response = requests.get('http://%s'%(url))
 print('Status Code: ', response.status_code)
 print('Cabeçalhos: ', response.headers['content-type'])
 print('Tamanho da resposta: ', response.headers['content-length'])
-print('Corpo da resposta: \n', response.text)
+#print('Corpo da resposta: \n', response.text)
 
 #---------------------------------------------------------
 
-print('----- Questão 02 -----\n')
+print('\n\n----- Questão 02 -----\n')
 
+# link da imagem:  https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/7/0/8/b708789ed8080c2c84c2e2fef367b1d5.jpg
+url = str(input('Insira a url\n>>>  '))
+response = requests.get(url)
+
+with open("imagem_baixada.jpg", "wb") as code:
+    code.write(response.content)
