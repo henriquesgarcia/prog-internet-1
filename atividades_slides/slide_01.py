477c51157b99b1ebe653bbad43f99072eb6de8fe
Henrique Garcia <enriqe_garcia15@hotmail.com>
2019-03-26 15:45:50 -0300

    questao 02 e inclusão de funções

--- atividades_slides/slide_01.py
+++ atividades_slides/slide_01.py
@@ -1,23 +1,63 @@
 # coding: utf-8
 import requests
 
-print('----- Questão 01 -----\n')
 
-url = str(input('Insira a url\nEx: www.uol.com\n>>>  '))
-response = requests.get('http://%s'%(url))
+def questao_03():
+    print('\n----- Questão 03 -----\n')
 
-print('Status Code: ', response.status_code)
-print('Cabeçalhos: ', response.headers['content-type'])
-print('Tamanho da resposta: ', response.headers['content-length'])
-#print('Corpo da resposta: \n', response.text)
+    url = str(input('Insira a url:\n>>>  '))
+    response = requests.get(url)
 
-#---------------------------------------------------------
 
-print('\n\n----- Questão 02 -----\n')
+def questao_02():
 
-# link da imagem:  https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/7/0/8/b708789ed8080c2c84c2e2fef367b1d5.jpg
-url = str(input('Insira a url\n>>>  '))
-response = requests.get(url)
+    print('\n\n----- Questão 02 -----\n')
 
-with open("imagem_baixada.jpg", "wb") as code:
-    code.write(response.content)
+    # link da imagem:  https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/7/0/8/b708789ed8080c2c84c2e2fef367b1d5.jpg
+
+    url = str(input('Insira a url\n>>>  '))
+    response = requests.get(url)
+
+    with open("/Users/enriq/Downloads/imagem_baixada.jpg", "wb") as arquivo:
+        arquivo.write(response.content)
+
+
+def questao_01():
+
+    print('\n----- Questão 01 -----\n')
+
+    url = str(input('Insira a url\nEx: www.uol.com\n>>>  '))
+    response = requests.get('http://%s'%(url))
+
+    print('Status Code: ', response.status_code)
+    print('Cabeçalhos: ', response.headers['content-type'])
+    print('Tamanho da resposta: ', len(response.headers['content-length']))
+    print('Corpo da resposta: \n', response.text)
+
+
+def menu():
+
+    print('\n----- Atividade do Slide 01 -----\n')
+    print('1 - Questão 01\n2 - Questão 02\n3 - Questão 03')
+
+
+def main():
+
+    while True:
+        
+        menu()
+        opcao = int(input('>>> '))
+
+        if opcao == 1:
+            questao_01()
+        elif opcao == 2:
+            questao_02()
+        elif opcao == 3:
+            print('fim')
+        else:
+            print('Finalizando...\n')
+            quit()
+
+
+if __name__ == "__main__":
+    main()
