430145549cf0ca7b21be1ab56c22671c7d76d9af
Henrique Garcia <enriqe_garcia15@hotmail.com>
2019-03-26 09:15:51 -0300

    questao 02

--- atividades_slides/slide_01.py
+++ atividades_slides/slide_01.py
@@ -1,7 +1,17 @@
 # coding: utf-8
 import requests
 
-url = str(input('Insira a url\nEx: www.uol.com.br\n>>>  '))
+print('----- Questão 01 -----\n')
+
+url = str(input('Insira a url\nEx: www.uol.com\n>>>  '))
 response = requests.get('http://%s'%(url))
 
-print(response.status_code)
+print('Status Code: ', response.status_code)
+print('Cabeçalhos: ', response.headers['content-type'])
+print('Tamanho da resposta: ', response.headers['content-length'])
+print('Corpo da resposta: \n', response.text)
+
+#---------------------------------------------------------
+
+print('----- Questão 02 -----\n')
+
